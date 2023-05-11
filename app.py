271d4fd5223c7df4477c539c2ac1146635378694
stephank007 <stephank@orantech.co.il>
2023-05-11 11:09:52 +0300

    initial load

--- app.py
+++ app.py
@@ -4,11 +4,8 @@ import dash_bootstrap_components as dbc
 from flask import Flask
 from dash import Dash, dcc, html, Input, Output, State
 
-server = Flask(__name__)
-
 app = Dash(
     title='heroku-sample',
-    server=server,
     suppress_callback_exceptions=True,
     prevent_initial_callbacks=True
 )
@@ -38,5 +35,7 @@ app.layout = dbc.Container(
     fluid=True,
 )
 
+server = app.server
+
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
