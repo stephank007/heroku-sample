27c67b1f1d7b52763a1c7a8cc3bebf93cb06ff64
stephank007 <stephank@orantech.co.il>
2023-05-11 10:14:28 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import dash
+import warnings
+import dash_bootstrap_components as dbc
+from flask import Flask
+from dash import Dash, dcc, html, Input, Output, State
+
+server = Flask(__name__)
+
+app = Dash(
+    title='Task Manager',
+    server=server,
+    suppress_callback_exceptions=True,
+    prevent_initial_callbacks=True
+)
+
+alerts = html.Div(
+    [
+        dbc.Alert(
+            [
+                "This is a primary alert with an ",
+                html.A("example link", href="#", className="alert-link"),
+            ],
+            color="primary",
+        ),
+        dbc.Alert(
+            [
+                "This is a danger alert with an ",
+                html.A("example link", href="#", className="alert-link"),
+            ],
+            color="danger",
+        ),
+    ]
+)
+app.layout = dbc.Container(
+    [
+        alerts
+    ],
+    fluid=True,
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
